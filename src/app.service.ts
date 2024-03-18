5d32ce2e7d073a7d612be0e26657d1028816c0ca
himanshi-rathore <108826224+himanshi-rathore@users.noreply.github.com>
2024-03-18 23:20:55 +0530

    changes made

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,24 @@
 //performs some task and returns back to controllers
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { IUser } from "./interface/user.interface";
+import { CreateUserDto } from "./dto/user.dto";
 
-@Injectable({})
-export class AppService{}
+@Injectable()
+export class AppService{
+    constructor(@InjectModel('User') private userModel:Model<IUser>) { }
+
+    async createUser(createUserDto: CreateUserDto): Promise<IUser> {
+        const newUser = await new this.userModel(createUserDto);
+        return newUser.save();
+     }
+
+     async getAllUser(): Promise<IUser[]> {
+        const userData = await this.userModel.find();
+        if (!userData || userData.length == 0) {
+            throw new NotFoundException('Users data not found!');
+        }
+        return userData;
+    }
+}
