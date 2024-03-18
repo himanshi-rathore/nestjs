f4d268d7e17d3971c1cc9b6f9b79ac0f4339b69c
himanshi-rathore <108826224+himanshi-rathore@users.noreply.github.com>
2024-03-18 23:21:18 +0530

    changes made

---
+++ src/dto/user.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsString} from "class-validator";
+export class CreateUserDto {
+    @IsString()
+   
+    @IsNotEmpty()
+    readonly firstName: string;
+
+    @IsString()
+   
+    @IsNotEmpty()
+    readonly lastName: string;
+
+    @IsString()
+   
+    @IsNotEmpty()
+    readonly password: string;
+   
+}
